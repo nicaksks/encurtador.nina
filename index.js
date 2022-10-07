f14f965f3a230a10fe0fe7485ca2bcc11c1f85e9
nicaksks <100009274+nicaksks@users.noreply.github.com>
2022-10-07 12:42:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await url.findOne({ short: req.params.shortUrl });
   if(shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
   shortUrl.save();
 
   res.redirect(shortUrl.full);
